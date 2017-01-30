5dcb6061249a6b6cb7bc1d2a664e1b25bb2e9c36
ivansivoplyas <ivan.kozaks@gmail.com>
2017-01-30 21:34:27 +0200

    Fixed compare method for two Numbers

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Andrew on 30.10.2016.
+ */
+public class Main {
+
+    public static void main (String[] args) throws Exception {
+        Number number1, number2;
+        number1 = new Number("3141592653589793238462643383279502884197169399375105820974944592");
+        number2 = new Number("2718281828459045235360287471352662497757247093699959574966967627");
+
+        System.out.println(FastMath.multiply(number1,number2) + " multiplication result");
+
+    }
+
+}
